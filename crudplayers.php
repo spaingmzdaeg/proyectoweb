0ebd3638031b9c5dca27ac66eea353a2032883cf
spainchesterdaeg <spainchester@outlook.com>
2020-11-03 01:35:12 -0600

    todavia funciona

--- crudplayers.php
+++ crudplayers.php
@@ -57,6 +57,7 @@ include_once 'playerstable.php';
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <script src="js/script.js"></script>
+    
   </div>
   <div id="overlay" style="display:none;">
     <div class="spinner-border text-danger" style="width: 3rem; height: 3rem;"></div>
