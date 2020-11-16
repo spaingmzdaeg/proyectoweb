8d216083b83bd6bd283e6527b3abc763185a6962
spainchesterdaeg <spainchester@outlook.com>
2020-11-16 14:24:33 -0600

    js validadado

--- js/scriptsignup.js
+++ js/scriptsignup.js
@@ -9,80 +9,106 @@
 }
 }*/
 
+function emailIsValid (email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
 
+
+
+function validateForm() {
+  var username = document.forms["addformuser"]["username"].value;
+  var email = document.forms["addformuser"]["email"].value;
+  var password = document.forms["addformuser"]["password"].value;
+  var confpassword = document.forms["addformuser"]["confpassword"].value;
+  var exampleCheck1 = document.forms["addformuser"]["exampleCheck1"].value;
+  
+  if (username == "") {
+    alert("username must be filled out");
+    return false;
+  }else if(username.length < 10){
+    alert(" please min 10 characters ");
+    return false;
+  }else if(username.length > 20){
+    alert(" please max 20 characters ");
+    return false;
+  }else if(email == ""){
+    alert("email must be filled out");
+    return false;
+  }else if(!emailIsValid(email)){
+    alert("invalid email!");
+    return false;
+  }else if(password.length < 10){
+    alert(" please min 10 characters ");
+    return false;
+  }else if(password.length > 20){
+    alert(" please max 20 characters ");
+    return false;
+  }else if(password == ""){
+    alert("password must be filled out");
+    return false;
+  }else if(confpassword == ""){
+    alert("confirm your password");
+    return false;
+  }else if(password != confpassword){
+    alert(" Passwords must be the same! ");
+    return false;
+  }
+  else if(!exampleCheck1.checked){
+    alert("check de terms");
+    return false;
+  }else {
+    return true;
+  }
+}
 
 
 
 $('form').on('submit', function(e) {
-  $(document).ready(function() {
-    $("#addformuser").validate({
-      rules: {
-        username : {
-          required: true,
-          minlength: 10,
-          maxlength:20
-        },
-        email: {
-          required: true,
-          email: true
-        },
-        password: {
-          required: true,
-          minlength: 10,
-          maxlength:20
-        },
-        confpassword:{
-          required: true,
-          minlength: 10,
-          maxlength:20
-        },
-        exampleCheck1:{
-          required: true
-        }
-      }
-    });
-  });
   
-  if(grecaptcha.getResponse() == "") {
-    e.preventDefault();
-    alert("You can't proceed! FILL CAPTCHA");
-  } else {
-    event.preventDefault();
-      var alertmsg =
-        $("#addbuttonuser").val().length > 0
-          ? "User has been updated Successfully!"
-          : "New User has been added Successfully!";
-
-      $.ajax({
-        url: "/phpcrudajaxspain/ajaxusers.php",
-        type: "POST",
-        dataType: "text",
-        data: new FormData(this),
-        processData: false,
-        contentType: false,
-        beforeSend: function () {
-          $("#overlay").fadeIn();
-        },
-        success: function (response) {
-          console.log(response);
-          if (response) {
-            
-            $("#addformuser")[0].reset();
-            $(".message").html(alertmsg).fadeIn().delay(3000).fadeOut();
-           // getplayers();
-            $("#overlay").fadeOut();
-            document.location.href = 'index.php';
-            
-          }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-          console.log(jqXHR);
-                  console.log(textStatus);
-                  console.log(errorThrown);
-        },
-      });
-    alert("User CREATED");
+  if(validateForm()){
+    if(grecaptcha.getResponse() == "") {
+      e.preventDefault();
+      alert("You can't proceed! FILL CAPTCHA");
+    } else {
+      event.preventDefault();
+        var alertmsg =
+          $("#addbuttonuser").val().length > 0
+            ? "User has been updated Successfully!"
+            : "New User has been added Successfully!";
+  
+        $.ajax({
+          url: "/phpcrudajaxspain/ajaxusers.php",
+          type: "POST",
+          dataType: "text",
+          data: new FormData(this),
+          processData: false,
+          contentType: false,
+          beforeSend: function () {
+            $("#overlay").fadeIn();
+          },
+          success: function (response) {
+            console.log(response);
+            if (response) {
+              
+              $("#addformuser")[0].reset();
+              $(".message").html(alertmsg).fadeIn().delay(3000).fadeOut();
+             // getplayers();
+              $("#overlay").fadeOut();
+              document.location.href = 'index.php';
+              
+            }
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+                    console.log(textStatus);
+                    console.log(errorThrown);
+          },
+        });
+      alert("User CREATED");
+    }
   }
+  
+  
 });
 
 
